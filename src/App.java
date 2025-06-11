120b2edcf5b51d2c566c8bf0a365fa816ece0bdd
Victoria Andrade <vandradev2@est.ups.edu.ec>
2025-06-11 08:42:43 -0500

    Clase 11 junio

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import model.Contact;
+
 public class App {
     public static void main(String[] args) throws Exception {
         controller.MenuController menuController = new controller.MenuController();
         menuController.showMenu();
+
+        Contact c1 = new Contact("Juan", "123");
+        Contact c4 = new Contact("Juan");
     }
 }
--- src/model/Contact.java
+++ src/model/Contact.java
@@ -1,33 +1,50 @@
 package model;
 
-public class Contact <T, U> {
-    private T name;
-    private U phone;
+public class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(){
+
+    }
     
-    public Contact(T name, U phone) {
+    public Contact(String name, String phone) {
         this.name = name;
         this.phone = phone;
     }
 
-    public T getName() {
+    public String getName() {
         return name;
     }
 
-    public void setName(T name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public U getPhone() {
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(U phone) {
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 
     @Override
     public String toString() {
         return "Contact [name=" + name + ", phone=" + phone + "]";
+    }
+
+    public boolean equals(Object obj){
+        if(obj == null) return false;
+        if(this == obj) return true; //this es la clase una vez instanseada 
+        if(obj == null || getClass() != obj.getClass()){// si contacto es diferente de la clase persona, retorna falso
+            return false;
+        }
+
+        //aqui obj y this son clase contacto
+        Contact c = (Contact) obj; //casting 
+        //c.phone; //ahora puedo acceder a las variables que tengo en la clase contacto
+        return name != null && name.equals(c.getName());
     }
     
 
